7eeaee654a8c21552fa09dc0e889644ecd525fe6
Felipe <145813128+FelipeLucas16@users.noreply.github.com>
2025-03-19 19:26:55 -0300

    criando projeto

---
+++ src/main/java/com/devffl/babershop/BabershopApplication.java
@@ -0,0 +1,13 @@
+package com.devffl.babershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabershopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devffl/babershop/BabershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devffl.babershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
